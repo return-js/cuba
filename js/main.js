83f3e7f29f34f2c551039984aeb71aafa4ef5938
Rerurn <return.work@mail.ru>
2022-07-29 10:53:19 +0300

    fix arrow

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
 
     $('.photo__slider').slick({
-        prevArrow: '<button type="button" className="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-        nextArrow: '<button type="button" className="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
     });
 
 });
